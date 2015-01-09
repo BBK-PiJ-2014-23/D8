b3b48acd1624a5f6284dec53a43a98d65b39ee98
unknown <Stefanm@LONWRK07474.eidos.com>
2015-01-09 16:26:26 +0000

    Merged with old class PersonNode.

--- SupermarketQueue/Person.java
+++ SupermarketQueue/Person.java
@@ -1,11 +1,21 @@
 public class Person {
-    String name;
+    private String name;
+    private Person next;
     
     public Person(String name) {
         this.name = name;
+        this.next = null;
     }
     
     public String getName() {
         return name;
+    }
+    
+    public void setNext(Person person) {
+        next = person;
+    }
+    
+    public Person getNext() {
+        return next;
     }
 }
