75415bdaf28bb3500a028c9202951a7c4fdc21c3
unknown <Stefanm@LONWRK07474.eidos.com>
2015-01-09 11:06:30 +0000

    First commit.

---
+++ Maps/StringNode.java
@@ -0,0 +1,26 @@
+public class StringNode {
+    int key;
+    String name;
+    StringNode next;
+    
+    public StringNode(String name) {
+        this.key = HashUtilities.shortHash(name.hashCode());
+        this.name = name;
+    }
+    
+    public int getKey() {
+        return key;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setNext(StringNode next) {
+        this.next = next;
+    }
+    
+    public StringNode getNext() {
+        return next;
+    }
+}
